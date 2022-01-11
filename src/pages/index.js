dfbd174ac554d8364b4cb707e97f57117a12aef7
Angelina Shalupova <shalupova.a@husky.neu.edu>
2022-01-11 15:56:08 -0500

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import React from "react"
 import './index.css'
 import AboutComponent from "../components/about"
-import NavBarComponent from "../components/navbar";
 import {Container, Grid} from "@material-ui/core"
 import MeComponent from "../components/me";
-import SkillsComponent from "../components/skills";
 import SocialsComponent from "../components/socials";
+import ProjectsComponent from "../components/projects";
 
 export default function Home() {
   return <Container fixed id="home">
-    <NavBarComponent/>
     <br/>
     <Grid
         container
@@ -21,28 +19,39 @@ export default function Home() {
           justify="space-evenly"
           alignItems="center"
           spacing={7}>
-          <Grid item lg align="right">
+          <Grid item lg align="center">
             <MeComponent/>
           </Grid>
-          <Grid item lg align="left">
+          <Grid item lg align="center">
             <p className="tagline">hello! i'm angelina—
               student and aspiring developer.</p>
+            <SocialsComponent/>
           </Grid>
       </Grid>
       <Grid
         container
         item
         justify="space-evenly" spacing={7}>
-        <Grid item lg align="right">
-          <SkillsComponent/>
-          <br/>
-          <SocialsComponent/>
+        <Grid item lg align="center">
+          <p className="title">about me</p>
         </Grid>
-        <Grid item lg align="left">
+        <Grid item lg align="center">
           <AboutComponent/>
         </Grid>
       </Grid>
     </Grid>
-    {/*<ProjectsComponent/>*/}
+    <Grid
+      container
+      item
+      justify="space-evenly"
+      alignItems="center"
+      spacing={7}>
+      <Grid item lg align="center">
+        <p className={"title"}>projects</p>
+      </Grid>
+      <Grid item lg align="center">
+        <ProjectsComponent/>
+      </Grid>
+    </Grid>
   </Container>
 }
