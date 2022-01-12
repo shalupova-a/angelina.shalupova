ef2fc7b0981fe53bf03fa9b27a0c62f12b6463c0
Angelina Shalupova <shalupova.a@husky.neu.edu>
2022-01-12 01:53:09 -0500

    Change projects component styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,13 +5,13 @@ import {IconButton, Typography, Accordion, AccordionDetails, AccordionSummary} f
 import {ExpandMore} from "@material-ui/icons";
 
 const AccordionItem = (props) =>
-        <Accordion>
+        <Accordion style={{backgroundColor: "#f7fcff", width: '85%'}}>
             <AccordionSummary
                 expandIcon={<ExpandMore />}
             >
                 <Typography>{props.title}</Typography>
             </AccordionSummary>
-            <AccordionDetails style={{display: "flex",
+            <AccordionDetails align="left" style={{display: "flex",
                 flexDirection: "column"}}>
                 <Typography variant={"subtitle2"}>
                     {props.language}
@@ -30,7 +30,7 @@ const ProjectsComponent = () =>
             <AccordionItem title={"personal website"} language={"GatsbyJS, MaterialUI, HTML/CSS, JS"} body={"You're looking at it! For this project, my goals were to create a simple, one-page website that is both\n" +
             "desktop and mobile-friendly (WIP)."} link={"https://github.com/shalupova-a/angelina.shalupova"}/>
             <AccordionItem title={"corvus web app"} language={"HTML/CSS, JS, ReactJS, Redux, Java, Bootstrap"} body={"This project started as a group effort for CS4550 - Web Development, and I have been working independently to improve it.\n" +
-            "Combining my love for astrology and music, this website recommends songs based on one's astrological sun sign!"} link={""}/>
+            "Combining my love for astrology and music, this website recommends songs based on one's astrological sun sign!"} link={"https://github.com/shalupova-a/corvus"}/>
             <AccordionItem title={"snarl game"} language={"Java"} body={"This project was a group effort for CS4500 - Software Development. This is a server-client 2D dungeon crawler game!"} link={"https://github.com/shalupova-a/Snarl"}/>
     </div>
 
