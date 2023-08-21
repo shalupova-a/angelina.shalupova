0c5fecb3de24e6f3cc43e63133405e43ef5086b1
Angelina <shalupova.a@husky.neu.edu>
2023-08-20 23:33:04 -0400

    add divider

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,26 +1,29 @@
 import React from "react";
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import { Divider } from '@mui/material';
 
 import Socials from './socials';
 
 const Landing = () =>
+    <Grid container
+        sx={{ marginTop: '5%' }} justify="space-evenly" spacing={3}>
+        <Grid>
+            <Divider orientation="vertical" className="divider" />
+        </Grid>
+        <Grid>
+            <Grid align="left">
+                <Grid>
+                    <p className="tagline">Hello there! I'm Angelina</p>
+                </Grid>
+            </Grid>
+            <Grid align="left" xs={6}>
+                <p>I’m a full-stack developer based in the Bay Area and recent alum of Northeastern University. Let’s connect!</p>
+            </Grid>
+            <Grid align="left" xs={7}>
+                <Socials />
+            </Grid>
 
-    <Grid
-        container
-        item
-        sp
-        sx={{ marginTop: '5%' }}
-    >
-        <Grid item align="left">
-            <p className="tagline">Hello there! I'm Angelina</p>
         </Grid>
-        <Grid item align="left" xs={6}>
-            <p>I’m a full-stack developer based in the Bay Area and recent alum of Northeastern University. Let’s connect!</p>
-        </Grid>
-        <Grid item align="left" xs={7}>
-            <Socials />
-        </Grid>
-
     </Grid>
 
 export default Landing
