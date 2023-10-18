858b7d08ab657f1b8aed4529f3783c4085a405b5
Angelina <shalupova.a@husky.neu.edu>
2023-10-18 01:22:30 -0400

    remove unnecessary grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import './index.css'
 import About from "../components/about"
 import { Container } from "@mui/material"
-import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Projects from "../components/projects";
 import Nav from "../components/nav";
 import Landing from "../components/landing";
@@ -12,27 +11,7 @@ export default function Home() {
     <Container>
       <Nav />
       <Landing />
-      <Grid
-        container
-        justify="space-evenly" spacing={3}>
-        <Grid>
-          <p className="title">about me</p>
-        </Grid>
-        <Grid>
-          <About />
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        justify="space-evenly"
-        alignItems="center"
-        spacing={3}>
-        <Grid>
-          <p className={"title"}>projects</p>
-        </Grid>
-        <Grid align="left">
-          <Projects />
-        </Grid>
-      </Grid>
+      <About />
+      <Projects />
     </Container >);
 }
