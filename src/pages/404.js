b1070c5d9fdc8ad72835ac3903064c49eb442288
Angelina <shalupova.a@husky.neu.edu>
2023-10-18 01:19:06 -0400

    make 404 page better

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,27 @@
+import { Container, IconButton } from "@mui/material"
 import React from "react"
+import { Grid } from "@mui/material"
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-// TODO: make better
 const Error = () =>
-    <p>404</p>
+    <Container>
+        <Grid container
+            sx={{ marginTop: '5%' }} justify="space-evenly" spacing={3}>
+            <Grid>
+                <Grid item>
+                    <p className="tagline">Uh oh...</p>
+                </Grid>
+                <Grid item>
+                    <p>There is nothing here :0</p>
+                </Grid>
+                <Grid item>
+                    <IconButton href="/" className="button">
+                        <FontAwesomeIcon icon={faArrowLeft} />
+                    </IconButton>
+                </Grid>
+            </Grid>
+        </Grid>
+    </Container>
 
 export default Error
