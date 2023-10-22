b0da192d81c372fedd94de67f9a05bed37aa25b3
Angelina <shalupova.a@husky.neu.edu>
2023-10-22 18:40:40 -0400

    fix grid for mobile

--- src/components/projects.js
+++ src/components/projects.js
@@ -27,20 +27,20 @@ const CardItem = (props) =>
 const Projects = () =>
     <div id="projects">
         <Reveal>
-            <Grid container sx={{ marginTop: '5%' }}>
+            <Grid container sx={{ marginTop: '5%' }} justify="space-evenly" spacing={3}>
                 <Grid align="left" item xs={6}>
                     <p className="title">projects</p>
                 </Grid>
-                <Grid container xs={6} spacing={3}>
-                    <Grid item xs={6}>
+                <Grid container xs={12} sm={6} spacing={3}>
+                    <Grid item xs={12} sm={6}>
                         <CardItem title={"Personal Website"} language={"GatsbyJS, MaterialUI, React, HTML/CSS, JS"} body={"You're looking at it! For this project, my goals were to create a simple, one-page website that is both\n" +
                             "desktop and mobile-friendly (WIP)."} link={"https://github.com/shalupova-a/angelina.shalupova"} />
                     </Grid>
-                    <Grid item xs={6}>
+                    <Grid item xs={12} sm={6}>
                         <CardItem title={"Corvus"} language={"HTML/CSS, JS, ReactJS, Redux, Java, Bootstrap"} body={"This project started as a group effort for CS4550 - Web Development.\n" +
                             "This website recommends songs based on one's astrological sun sign!"} link={"https://github.com/shalupova-a/corvus"} />
                     </Grid>
-                    <Grid item xs={6}>
+                    <Grid item xs={12} sm={6}>
                         <CardItem title={"Snarl"} language={"Java"} body={"This project was a group effort for CS4500 - Software Development. This is a server-client 2D dungeon crawler game!"} link={"https://github.com/shalupova-a/Snarl"} />
                     </Grid>
                 </Grid>
